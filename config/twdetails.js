88204e6fc6908a0f8a1a1be3c1c6e4d745c06a38
Kenn Pascascio <kenn370@gmail.com>
2018-03-07 09:11:50 -0500

    deleted config files

--- config/twdetails.js
+++
@@ -1,8 +0,0 @@
-const twilio = require('twilio');
-
-twilioNumber = '(321) 250-2962'
-
-module.exports = {
-    twilio: new twilio(process.env.TWACCOUNTSID, process.env.TWAUTHTOKEN),
-    twilioNumber
-}
